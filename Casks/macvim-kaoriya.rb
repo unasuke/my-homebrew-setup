b4521ff71b24508811a9eee79fb8ee70620dee03
Nakamura Yusuke <yusuke1994525@gmail.com>
2014-03-22 23:53:06 +0900

    remove Casks

--- Casks/macvim-kaoriya.rb
+++
@@ -1,7 +0,0 @@
-class MacVimKaoriya < Cask
-  url 'https://github.com/splhack/macvim/releases/download/20140107/macvim-kaoriya-20140107.dmg'
-  homepage 'https://code.google.com/p/macvim-kaoriya/'
-  version '20140107'
-  sha256 '0a87ec0cb28e3b40eec6deb90e9e41ba44937f80de0c154cf80865414a6900b3'
-  link 'MacVim.app'
-end
